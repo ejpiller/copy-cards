0e9154996a0eee287b178ced11b1abd75548a087
Erick Piller <ejpiller@gmail.com>
2023-01-02 15:48:35 -0600

    Various updates

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const app = document.getElementById("app");
 
 function createCard() {
-  const el = document.createElement("div");
-  el.setAttribute("class", "item card");
-  el.setAttribute(
-    "onclick",
-    "console.log(navigator.clipboard.writeText(this.innerText));"
-  );
-  el.innerText = document.getElementById("textarea").value;
-  app.appendChild(el);
-  document.getElementById("textarea").value = "";
+  let input = document.getElementById("textarea").value;
+  console.log(input);
+  if (input == "") {
+    console.log("You must enter text to create a card.");
+  } else {
+    const el = document.createElement("div");
+    el.setAttribute("class", "item card");
+    el.setAttribute(
+      "onclick",
+      "console.log(navigator.clipboard.writeText(this.innerText));"
+    );
+    el.innerText = document.getElementById("textarea").value;
+    app.appendChild(el);
+    document.getElementById("textarea").value = "";
+  }
 }
