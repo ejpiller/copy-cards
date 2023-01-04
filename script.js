477941e2a4c4297464d95611b452f122ea7aab98
Erick Piller <ejpiller@gmail.com>
2023-01-04 06:09:23 -0600

    Trim whitespace

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const app = document.getElementById("app");
 
 function createCard() {
   let input = document.getElementById("textarea").value;
-  if (input == "") {
+  if (input.trim() == "") {
     console.log("You must enter text to create a card.");
   } else {
     const el = document.createElement("div");
-    el.innerText = document.getElementById("textarea").value;
+    el.innerText = document.getElementById("textarea").value.trim();
     el.setAttribute("class", "item card");
     el.onclick = copyText;
     app.appendChild(el);
